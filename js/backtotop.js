7d1aa5ce86052111c9c0ef6236dd8b54e1504939
Art 3870-02 <art3870-02@Arts-194-108.local>
2018-05-30 17:42:40 -0700

    btn

--- js/backtotop.js
+++ js/backtotop.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 30700 || document.documentElement.scrollTop > 30700) {
+    if (document.body.scrollTop > 28700 || document.documentElement.scrollTop > 28700) {
         document.getElementById("myBtn").style.display = "block";
     } else {
         document.getElementById("myBtn").style.display = "none";
